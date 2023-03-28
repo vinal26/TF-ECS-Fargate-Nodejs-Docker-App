adc9fe187c54df9c20ff9a5293853b2d0caab96b
vinal26 <vinalpatel26.vp@gmail.com>
2023-03-28 13:58:55 +0530

    Working Terraform and Docker Script for AWS ECS Fargate

---
+++ src/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 80;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello there from test-nodejs-app!!');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
